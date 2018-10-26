5743144a1d1cad2dc13247235e0f50bb5828466a
Dean <dean.shorter@orchard-systems.co.uk>
2018-10-26 13:35:10 +0100

    refactored random number gen

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
-let rollBtn = document.querySelector('.rollBtn');
+let rollBtn = document.querySelector('.roll-btn');
 let diceNumber = document.querySelector('.number');
 let numberDisplay = document.querySelector('#number-display')
 let numbersOnDice = [1, 2, 3, 4, 5, 6];
 let dice = document.querySelector('.dice');
 
 
+
 rollBtn.addEventListener('click', rollTheDice);
 
 
-
 function rollTheDice() {
-
-    let randomNumber = Math.floor(Math.random() * (numbersOnDice.length + 1));
-    console.log(randomNumber);
+        
+    let randomNumber = Math.floor(1+ Math.random() * (numbersOnDice.length));
+    
     if (randomNumber == 1) {
         dice.style.backgroundImage = "url(images/Dice1.png)";
+        numberDisplay.innerHTML = randomNumber;
     } else if (randomNumber == 2) {
         dice.style.backgroundImage = "url(images/Dice2.png)";
+        numberDisplay.innerHTML = randomNumber;
     } else if (randomNumber == 3) {
         dice.style.backgroundImage = "url(images/Dice3.png)";
+        numberDisplay.innerHTML = randomNumber;
     } else if (randomNumber == 4) {
-        dice.style.backgroundImage = "url(images/Dice4.png)"
+        dice.style.backgroundImage = "url(images/Dice4.png)";
+        numberDisplay.innerHTML = randomNumber;
     } else if (randomNumber == 5) {
-        dice.style.backgroundImage = "url(images/Dice5.png)"
+        dice.style.backgroundImage = "url(images/Dice5.png)";
+        numberDisplay.innerHTML = randomNumber;
     } else if (randomNumber == 6) {
-        dice.style.backgroundImage = "url(images/Dice6.png)"
-    } else if(randomNumber == 0){
-        rollTheDice();
+        dice.style.backgroundImage = "url(images/Dice6.png)";
+        numberDisplay.innerHTML = randomNumber;
     }
 
-    numberDisplay.innerHTML = randomNumber;
+
+    
     
 }

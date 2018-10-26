c52d66ababced4272f273b07bdb7b31adcccb611
Dean <dean.shorter@orchard-systems.co.uk>
2018-10-26 11:59:48 +0100

    First Edition

---
+++ app.js
@@ -0,0 +1,34 @@
+let rollBtn = document.querySelector('.rollBtn');
+let diceNumber = document.querySelector('.number');
+let numberDisplay = document.querySelector('#number-display')
+let numbersOnDice = [1, 2, 3, 4, 5, 6];
+let dice = document.querySelector('.dice');
+
+
+rollBtn.addEventListener('click', rollTheDice);
+
+
+
+function rollTheDice() {
+
+    let randomNumber = Math.floor(Math.random() * (numbersOnDice.length + 1));
+    console.log(randomNumber);
+    if (randomNumber == 1) {
+        dice.style.backgroundImage = "url(Dice1.png)";
+    } else if (randomNumber == 2) {
+        dice.style.backgroundImage = "url(Dice2.png)";
+    } else if (randomNumber == 3) {
+        dice.style.backgroundImage = "url(Dice3.png)";
+    } else if (randomNumber == 4) {
+        dice.style.backgroundImage = "url(Dice4.png)"
+    } else if (randomNumber == 5) {
+        dice.style.backgroundImage = "url(Dice5.png)"
+    } else if (randomNumber == 6) {
+        dice.style.backgroundImage = "url(Dice6.png)"
+    } else if(randomNumber == 0){
+        rollTheDice();
+    }
+
+    numberDisplay.innerHTML = randomNumber;
+    
+}
